c5cec52fca52078206cfc609af6f47418269338c
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2025-04-24 16:41:41 +0200

    Initial commit adding Laravel

---
+++ webpack.mix.js
@@ -0,0 +1,12 @@
+const mix = require('laravel-mix');
+const path = require('path');
+
+mix
+    .js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss'),
+    ])
+    .alias({
+        '@': path.join(__dirname, 'resources/js'),
+    })
+    .version();
