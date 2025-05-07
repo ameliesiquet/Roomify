6fe119402503ed0bb52f3353461e7774582dddb6
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2025-05-07 15:39:03 +0200

    logout and redirect to login page

--- resources/views/components/sidebar/bottom-links.blade.php
+++ resources/views/components/sidebar/bottom-links.blade.php
@@ -1,10 +1,17 @@
 @props([
     'expanded' => true,
 ])
-<ul class="font-medium flex flex-col items-center gap-8 text-center w-full" role="list">
-    <x-sidebar.link href="{{ route('profile') }}" icon="profile" label="Profile" :expanded="$expanded" />
-    <li>
-        <x-button>Logout</x-button>
+<ul class="font-medium flex flex-col items-center gap-8 text-center w-full lg:items-start" role="list">
+    <x-sidebar.link href="{{ route('profile') }}" icon="profile" label="Profile" :expanded="$expanded"/>
+    <li class="">
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <x-button>Logout</x-button>
+        </form>
     </li>
+    <button class="bg-turquoise hover:bg-gray-300 p-4 z-50">
+        Test Button
+    </button>
+
 </ul>
 
