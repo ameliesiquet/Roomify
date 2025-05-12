f1de0cc30c64e5e253f2ff96d815af92b4ef4b27
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2025-05-12 14:45:58 +0200

    refactor

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 @include('partials.head')
-<body class="font-sans bg-mywhite ">
-<main class="min-h-[100vh] bg-mywhite">
-    <p>Hallo (guest.blade)</p>
+<body class="font-sans bg-mywhite  ">
+<main class="px-4 mt-18 min-h-[100vh] bg-mywhite lg:mt-8 flex flex-col items-center">
     {{ $slot }}
 </main>
 @livewireScripts
