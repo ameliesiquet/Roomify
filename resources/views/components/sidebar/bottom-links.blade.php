528920a1334fd4d5aa328e8cc1d1ec7e39ecdcad
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2025-05-12 14:34:07 +0200

    update nav, add logout function

--- resources/views/components/sidebar/bottom-links.blade.php
+++ resources/views/components/sidebar/bottom-links.blade.php
@@ -1,6 +1,24 @@
 @props([
     'expanded' => true,
 ])
+<?php
+
+use App\Livewire\Actions\Logout;
+use Livewire\Volt\Component;
+
+new class extends Component
+{
+    /**
+     * Log the current user out of the application.
+     */
+    public function logout(Logout $logout): void
+    {
+        $logout();
+
+        $this->redirect('/', navigate: true);
+    }
+}; ?>
+
 <ul class="font-medium flex flex-col items-center gap-8 text-center w-full lg:items-start" role="list">
     <x-sidebar.link href="{{ route('profile') }}" icon="profile" label="Profile" :expanded="$expanded"/>
     <li class="">
