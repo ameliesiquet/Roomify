255989dc4772dcd7ceb7606c678d678de8fd1f3a
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2025-05-28 13:42:49 +0200

    add other page controllers

--- app/Livewire/Pages/Dashboard.php
+++ app/Livewire/Pages/Dashboard.php
@@ -6,15 +6,12 @@ use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 class Dashboard extends Component
 {
-    public function mount()
-    {
-        logger()->info('DASHBOARD User:', ['user' => Auth::user()]);
-    }
-
 
     public function render()
     {
         return view('livewire.pages.dashboard')
             ->layout('layouts.app-sidebar');
     }
+
+
 }
---
+++ app/Livewire/Pages/Profile.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire\Pages;
+
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class Profile extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.profile', [
+        ])->layout('layouts.app-sidebar');
+    }
+}
