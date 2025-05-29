479f956932538de988bd8d0082bcb08b4da73622
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2025-05-29 10:04:05 +0200

    Update dashboard.blade.php

--- resources/views/livewire/pages/dashboard.blade.php
+++ resources/views/livewire/pages/dashboard.blade.php
@@ -6,11 +6,9 @@
     @endif
 
     <section>
-
         <h2>Roomify</h2>
         <p>Hello 👋🏻You paused while adding items to your Bathroom 😮🛁</p>
         <img src="{{ asset('storage/' . Auth::user()->profile_photo_path) }}" alt="Profilbild" class="w-20 h-20 rounded-full">
-
     </section>
 
     <section>
