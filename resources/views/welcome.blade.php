7135da21f6f393ca16f11dfd7005e6354b495211
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2025-06-11 20:23:36 +0200

    add buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,4 +3,19 @@
     <section class="flex flex-col items-center justify-center">
         <h2 class="text-sm">A short explanation of this application:</h2>
     </section>
+    <div class="flex gap-3">
+        <a class="underline text-xs text-gray-600 hover:text-gray-900 rounded-md"
+           href="{{ route('login') }}">
+            <x-button class="ms-3">
+                {{ __('Log in') }}
+            </x-button>
+        </a>
+
+        <a class="underline text-xs text-gray-600 hover:text-gray-900 rounded-md"
+           href="{{ route('register') }}">
+            <x-button class="ms-3">
+                {{ __('Create an account') }}
+            </x-button>
+        </a>
+    </div>
 </x-guest-layout>
