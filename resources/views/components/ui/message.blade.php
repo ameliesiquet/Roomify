469116975c144cae4520ff69a96aa759c05f1da0
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2025-06-11 19:29:21 +0200

    update messages

--- resources/views/components/ui/message.blade.php
+++ resources/views/components/ui/message.blade.php
@@ -10,7 +10,6 @@
     </h2>
     <div class="flex flex-col items-start gap-1 bg-white border border-gray-200 rounded-xl p-4 shadow-sm">
         <p class="text-sm text-gray-800 whitespace-pre-line">{{ $message }}</p>
-
     @if($linkText && $linkHref)
             <a href="{{ $linkHref }}" class="text-turquoise hover:underline underline text-sm inline-flex items-center">
                 {{ $linkText }}
