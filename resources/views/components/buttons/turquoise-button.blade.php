bba403dbc8cc45bfac69ba5318b15da2ca5a5990
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2026-01-17 18:29:47 +0100

    update buttons

--- resources/views/components/buttons/turquoise-button.blade.php
+++ resources/views/components/buttons/turquoise-button.blade.php
@@ -1,6 +1,6 @@
 <button
     {{ $attributes->merge([
-        'class' => 'text-xs md:text-sm lg:text-base self-stretch px-2 py-1.5 text-white bg-turquoise rounded-lg cursor-pointer'
+        'class' => 'text-xs md:text-sm lg:text-sm self-stretch px-2 py-1.5 text-white bg-turquoise rounded-lg cursor-pointer'
     ]) }}
 >
     {{ $slot }}
